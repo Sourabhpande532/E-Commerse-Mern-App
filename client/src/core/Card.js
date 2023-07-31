75949622b431a907f84c8a390092a7a208ccd70d
sourabh <sourabhpande43@gmail.comm>
2023-07-31 15:54:14 +0530

    [fix] - image file

--- client/src/core/Card.js
+++ client/src/core/Card.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import ImageHelper from "./helper/ImageHelper";
+import ImageHelper from "./helper/imageHelper";
 import { addItemToCart, removeItemFromCart } from "./helper/cartHelper";
 
 const Card = ({
